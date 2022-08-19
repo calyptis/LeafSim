35c9bd8e4bb9d0a5094ecb316e14e2b523450e71
Chizzali Lucas <lucas.chizzali@epfl.ch>
2022-08-19 16:12:02 +0200

    updates

--- src/utils.py
+++ src/utils.py
@@ -42,8 +42,8 @@ def conditional_highlight(s: pd.Series, val: str) -> list[str]:
     """
     Returns single-colour background styling rows in the pandas series s, based on whether their values match val.
     
-    :param s:
-    :param val: Value to check against
+    :param s: Column to check if any entry is equal to val
+    :param val: Value entries in s should be equal to
     """
     mask = s == val
     return ['background-color: ' if i else 'background-color: lightcoral;' for i in mask]
@@ -56,8 +56,7 @@ def get_stylised_similar_cars_table(
     top_n_distances: np.ndarray,
     car_to_explain_id: int,
     top_n_features: list[str],
-    formatting: dict[str, str],
-    target_col: str = "price"
+    formatting: dict[str, str]
 ) -> pd.DataFrame:
     """
     Generates a stylised table of the most similar cars for a chosen car to be explained (car_to_explain_id).
@@ -74,13 +73,11 @@ def get_stylised_similar_cars_table(
     :param car_to_explain_id: Index of the test car to explain
     :param top_n_features: The list of feature columns to visualise in the stylised table
     :param formatting: Custom formatting of the feature columns passed on to pd.DataFrame().style.format(...)
-    :param target_col: The name of the target column
     :return: Stylised dataframe holding the top N most similar cars
     """
     d = df_test.loc[
         car_to_explain_id,
-        ["predicted_price", "mileage", "year", "enginesize", "brand",
-         "model", "fueltype", "transmission"]
+        top_n_features + ["predicted_price"]
     ].to_dict()
 
     df_to_show = (
@@ -89,9 +86,9 @@ def get_stylised_similar_cars_table(
         .assign(
             similarity=lambda x: 1 - top_n_distances[car_to_explain_id, :],
             # Measure the relative price difference between similar cars and the car to explain
-            # This column is used to colour columns actually shown in the table
+            # This column is used to colour the price column shown in the table
             diff_price=lambda x: (x.price - d["predicted_price"]).abs() / d["predicted_price"],
-            # Do the same as above for the other numeric attributes
+            # Do the same as above for other numeric attributes
             diff_mileage=lambda x: (x.mileage - d["mileage"]).abs() / d["mileage"],
             diff_year=lambda x: (x.year - d["year"]).abs() / d["year"],
             diff_enginesize=lambda x: (x.enginesize - d["enginesize"]).abs() / d["enginesize"]
@@ -102,14 +99,14 @@ def get_stylised_similar_cars_table(
 
     df_to_show = (
         df_to_show
-        [top_n_features + ["similarity", target_col]]
+        [top_n_features + ["similarity", "price"]]
         .style
         .format(formatting)
         # Add a background colour to the certain column based on how
-        # far away they are from the car to be predicted (in relative terms)
+        # far away they are from the car to explain (in relative terms)
         .apply(
             conditional_background_gradient, subset="price",
-            cmap="Reds", df=df_to_show, col="diff_price", upper_threshold=1
+            cmap="Reds", df=df_to_show, col=f"diff_price", upper_threshold=1
         )
         .apply(
             conditional_background_gradient, subset="mileage",
@@ -125,10 +122,11 @@ def get_stylised_similar_cars_table(
         )
         # Add a background gradient for the similarity column
         .background_gradient(subset="similarity", cmap="Blues", vmax=1, vmin=0)
-        # Highlight cells in the categorical columns if they differ from the value of the car to explain
+        # Highlight cells in categorical columns if they differ from the value of the car to explain
         .apply(conditional_highlight, val=d["brand"], subset=["brand"])
         .apply(conditional_highlight, val=d["fueltype"], subset=["fueltype"])
         .apply(conditional_highlight, val=d["brand"], subset=["brand"])
+        .apply(conditional_highlight, val=d["transmission"], subset=["transmission"])
         .apply(conditional_highlight, val=d["model"], subset=["model"])
     )
 
@@ -173,7 +171,7 @@ def get_similarity_plots(
                label="Car to explain - actual price")
     sns.lineplot(
         data=tmp, x="similarity", y="price", linewidth=1, color="darkviolet", alpha=0.5,
-        ci="sd", label="$\mu$ and $\sigma$ of car's prices in training set", ax=ax
+        ci="sd", label="$\mu$ and $\sigma$ of prices of cars in training set", ax=ax
     )
     ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f"{x:,.0f}"))
     ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f"{x * 100:,.0f}"))
@@ -186,7 +184,7 @@ def get_similarity_plots(
     sns.histplot(test_avg_similarity, ax=ax, stat="probability", color="darkviolet", alpha=0.5,
                  label="All cars in test set")
     ax.axvline(test_avg_similarity[car_to_explain_id], linewidth=1, c="royalblue", label="Car to explain")
-    ax.set_title("Distribution of Average of Top 50 LeafSim Scores")
+    ax.set_title("Distribution of Average Top 50 LeafSim Scores")
     ax.set_xlabel("Average Top 50 LeafSim Score [%]")
     ax.set_ylabel("Share [%]")
     ax.xaxis.set_major_formatter(ticker.FuncFormatter(lambda x, pos: f"{x * 100:,.0f}"))
